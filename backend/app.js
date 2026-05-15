a4269c63f9edd5c690c3746ac657c8a96d229edf
Amrendra Pratap Singh <amrendra727@gmail.com>
2026-05-15 17:01:44 +0530

    Moved frontend files to root

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const port = 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '../')));
 
 // Initialize Supabase REST client (no database password needed!)
 const supabase = createClient(
